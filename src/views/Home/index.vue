cb2458f34e4fbf0b5d52e18fef91a8cf0d393548
ks <1239686349@qq.com>
2023-07-21 08:59:13 +0800

    feat: add home page

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,10 @@
 <template>
   <el-card>
     <div class="box">
-      <div class="bottom">
-        <h3 class="title">好呀小牛</h3>
-        <p class="subtitle">硅谷甄选运营平台</p>
+      <img class="avatar" :src="useUserStore.avatar" alt="用户头像" />
+      <div class="show">
+        <h3 class="title">{{ getHour() }}好呀 {{ useUserStore.username }}</h3>
+        <p class="subtitle">new beginning</p>
       </div>
     </div>
   </el-card>
@@ -12,36 +13,33 @@
   </div> -->
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getHour } from '@/utils/time.ts'
+import userStore from '@/store/modules/user'
+// 引入用户信息store
+let useUserStore = userStore()
+</script>
 
 <style scoped lang="scss">
 .box {
   display: flex;
-
+  align-items: center;
   .avatar {
     width: 100px;
     height: 100px;
     border-radius: 50%;
   }
-
-  .bottom {
+  .show {
     margin-left: 20px;
-
     .title {
       font-size: 30px;
       font-weight: 900;
       margin-bottom: 30px;
     }
-
     .subtitle {
       font-style: italic;
       color: skyblue;
     }
   }
-}
-.bottoms {
-  margin-top: 10px;
-  display: flex;
-  justify-content: center;
 }
 </style>
